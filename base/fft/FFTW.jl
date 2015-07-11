99673e41ba34d010517d65781ba05fc7e8d92486
Yichao Yu <yyc1992@gmail.com>
2015-07-11 18:38:15 -0400

    Fix deprecation warning of FFTW.flops

--- base/fft/FFTW.jl
+++ base/fft/FFTW.jl
@@ -244,17 +244,17 @@ let add=[0.0], mul=[0.0], fma=[0.0]
         ccall((:fftw_flops,libfftw), Void,
               (PlanPtr,Ptr{Float64},Ptr{Float64},Ptr{Float64}),
               plan, add, mul, fma)
-        (int64(add[1]), int64(mul[1]), int64(fma[1]))
+        (round(Int64, add[1]), round(Int64, mul[1]), round(Int64, fma[1]))
     end
     function arithmetic_ops{T<:fftwSingle}(plan::FFTWPlan{T})
         ccall((:fftwf_flops,libfftwf), Void,
               (PlanPtr,Ptr{Float64},Ptr{Float64},Ptr{Float64}),
               plan, add, mul, fma)
-        (int64(add[1]), int64(mul[1]), int64(fma[1]))
+        (round(Int64, add[1]), round(Int64, mul[1]), round(Int64, fma[1]))
     end
 end
-flops(plan::FFTWPlan) = let ops=arithmetic_ops(plan)
-    ops[1] + ops[2] + 2*ops[3] # add + mul + 2*fma
+flops(plan::FFTWPlan) = let ops = arithmetic_ops(plan)
+    ops[1] + ops[2] + 2 * ops[3] # add + mul + 2*fma
 end
 
 # Pretty-printing plans
